ee31837556a29c545ce08f34afdabe0462c9d1f6
kauanzokixxx <kaualopesribeiro51@gmail.com>
2026-08-02 19:35:07 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
+import discord
+from discord.ext import commands
+import datetime
+import json
+import config
 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+with open("versiculos.json", "r", encoding="utf-8") as arquivo:
+    versiculos = json.load(arquivo)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online: {bot.user}")
+
+@bot.command()
+async def versiculo(ctx):
+    dia = datetime.datetime.now().timetuple().tm_yday
+    
+    if dia <= len(versiculos):
+        mensagem = versiculos[str(dia)]
+    else:
+        mensagem = "Deus abençoe seu dia! 🙏"
+
+    await ctx.send(
+        f"📖 **Versículo do Dia**\n\n{mensagem}\n\n"
+        "⚽ Blessing FC | 🙏 Deus abençoe"
+    )
+
+bot.run(config.TOKEN)
